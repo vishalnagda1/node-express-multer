9b7f784e06e20025d07d580ad6e1a337b3ce7ca7
Vishal Nagda <atom@Vishals-MacBook-Air.local>
2020-10-02 19:05:17 +0530

    register a file upload apis in main route file

--- routes.js
+++ routes.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
+const fileUpload = require("./src/file_upload/routes");
 
 /** GET / or GET /health-check - Check server health */
 router.get(["/", "/health-check"], (request, response) => {
   response.send("OK");
 });
+
+// mount file_upload routes at /files
+router.use("/upload", fileUpload);
 
 module.exports = router;
