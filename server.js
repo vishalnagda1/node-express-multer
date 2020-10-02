0e5290c057fe33f5b9cc25deb4e4c56160c41785
Vishal Nagda <atom@Vishals-MacBook-Air.local>
2020-10-02 15:56:33 +0530

    create a health check api

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
+
+// mounting main routes
+app.use(routes);
 
 const port = process.env.PORT || 3000;
 
